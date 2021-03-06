216af692eb5df9fb1fd757e105abf89fa668f6fe
김성일 <sung-ilkim@gimseong-ils-MacBook-Air.local>
2021-03-06 19:50:31 +0900

    Initial commit

---
+++ src/test/java/me/jaden/springbootcleanblog/SpringBootCleanBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jaden.springbootcleanblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCleanBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
